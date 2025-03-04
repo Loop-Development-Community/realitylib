dc3382f00a302f4ba5b8f03d3a134b2b64de50af
Siwakorn_Sep <team.loopdevcommunity@gmail.com>
2025-03-04 17:25:33 +0700

    Plugin Build

---
+++ src/main/kotlin/me/siwakornsep/realitylib/Realitylib.kt
@@ -0,0 +1,14 @@
+package me.siwakornsep.realitylib
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Realitylib : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
